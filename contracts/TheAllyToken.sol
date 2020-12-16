ad5e31333586eb7cdc9801eae7a740eceba6bc30
Abhishek Sinha <avishkarabhishek786@gmail.com>
2020-12-16 20:31:06 +0530

    migration success

--- contracts/TheAllyToken.sol
+++ contracts/TheAllyToken.sol
@@ -5,15 +5,6 @@ import '@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol';
 
-// contract TheAllyToken is MintableToken, PausableToken, DetailedERC20 {
-//     constructor(string memory _name, string _symbol, uint8 _decimals)
-//         DetailedERC20(_name, _symbol, _decimals)
-//         public
-//     {
-
-//     }
-// }
-
 contract TheAllyToken is ERC20Mintable, ERC20Pausable, ERC20Detailed {
     constructor(string memory _name, string memory _symbol, uint8 _decimals)
         ERC20Detailed(_name, _symbol, _decimals)
